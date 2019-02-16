388a37239201d97f672fb20b354062076ad36f7d
Riteshg15 <47108530+Riteshg15@users.noreply.github.com>
2019-02-16 15:47:05 +0530

    Update Arraylist1.java
    arraylist1 change

--- Programs/src/collection/Arraylist1.java
+++ Programs/src/collection/Arraylist1.java
@@ -3,7 +3,6 @@ package collection;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-//Wap to read all elements in arraylist by using an iterator.
 public class Arraylist1 {
 	public static void main(String[] args) {
 		ArrayList<String> al=new ArrayList<>();
